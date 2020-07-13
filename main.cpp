8ce3564efbb67df3bb50b705fd22458b63736d66
Madhusudan98 <49197031+Madhusudan98@users.noreply.github.com>
2020-07-13 18:17:48 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include<bits/stdc++.h>
-#include "XMLCheck.h"
-#include "Update_XML.h"
-#include "process_XML.h"
-#include "search_XML.h"
+#include "/headers/XMLCheck.h"
+#include "/headers/Update_XML.h"
+#include "/headers/process_XML.h"
+#include "/headers/search_XML.h"
 
 void print_tags(graph xml_grpah,int nested_level){
     if(xml_grpah.tag_list.size()==0){
